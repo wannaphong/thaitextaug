5bb3846c96e410a627c014bc5b6a9f2d51c8526c
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-03-28 00:34:02 +0700

    Update test_wordnet.py

--- tests/test_wordnet.py
+++ tests/test_wordnet.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import unittest
-from thaitextaug.wordnet import WordNet
+from thaitextaug.wordnet import WordNetAug
 
 
 class WordNetPackage(unittest.TestCase):
     def test_WordNet(self):
-        w = WordNet()
+        w = WordNetAug()
         text = "ประกาศมหาวิทยาลัย ฉบับที่ 6"
         self.assertIsNotNone(w.augment(text))
