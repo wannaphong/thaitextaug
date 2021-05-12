81649e18ca886815bfff6ff28ad38096e175eb7f
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-05-12 14:59:02 +0700

    Update fasttext.py

--- thaitextaug/lm/fasttext.py
+++ thaitextaug/lm/fasttext.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from typing import List
+from typing import List, Tuple
 from gensim.models.fasttext import FastText
 from pythainlp.tokenize import word_tokenize
 from gensim.models.keyedvectors import KeyedVectors
@@ -19,17 +19,17 @@ class FastTextAug:
             self.model = KeyedVectors.load_word2vec_format(model_path)
         else:
             self.model = FastText.load(model_path)
-    def tokenize(self, text: str):
+    def tokenize(self, text: str)-> List[str]:
         """
         Thai text tokenize for fasttext
 
         :param str text: thai text
 
         :return: list of word
-        :rtype: list
+        :rtype: List[str]
         """
         return word_tokenize(text, engine='icu')
-    def modify_sent(self,sent, p = 0.7):
+    def modify_sent(self,sent: list, p: float = 0.7) -> Tuple[str]:
         list_sent_new = []
         dict_wv = list(self.model.vocab.keys())
         for i in sent:
@@ -41,8 +41,8 @@ class FastTextAug:
                     list_sent_new.append(i)
             else:
                 list_sent_new.append(i)
-        return list_sent_new
-    def augment(self, sentence: str, n_sent: int = 1, p:float = 0.7) -> List[str]:
+        return tuple(list_sent_new)
+    def augment(self, sentence: str, n_sent: int = 1, p:float = 0.7) -> List[Tuple[str]]:
         """
         Text Augment from FastText
 
@@ -53,7 +53,7 @@ class FastTextAug:
         :param float p: Probability of word
 
         :return: list of synonyms
-        rtype: list
+        :rtype: List[Tuple[str]]
         """
         self.sentence = self.tokenize(sentence)
         self.temp = []
