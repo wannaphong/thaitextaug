367ca10f4c169e572f688da3f65f2d998e23fd7c
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-05-13 18:50:47 +0700

    Update wangchanberta.py

--- thaitextaug/lm/wangchanberta.py
+++ thaitextaug/lm/wangchanberta.py
@@ -37,13 +37,12 @@ class Thai2transformersAug:
     def generate(self, sentence: str, num_replace_tokens: int=3):
         self.sent2 = []
         self.input_text = process_transformers(sentence)
-        sent = self.tokenizer.tokenize(self.input_text)
-        sent.remove('▁')
-        if len(list(set(sent))) < num_replace_tokens:
-            num_replace_tokens = len(list(set(sent)))
+        sent = [i for i in self.tokenizer.tokenize(self.input_text) if i != '▁']
+        if len(sent) < num_replace_tokens:
+            num_replace_tokens = len(sent)
         masked_text = self.input_text
         for i in range(num_replace_tokens):
-            replace_token = random.choice(sent)
+            replace_token =  [sent.pop(random.randrange(len(sent))) for _ in range(1)][0]
             masked_text = masked_text.replace(replace_token, f"{self.fill_mask.tokenizer.mask_token}",1)
             self.sent2+=[str(j['sequence']).replace('<s> ','').replace('</s>','') for j in self.fill_mask(masked_text+'<pad>') if j['sequence'] not in self.sent2]
             masked_text = self.input_text
